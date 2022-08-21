b66a689c575e4b8ddbfb326576aa19c6d1fc8e2c
Denis Stoyanouski <vandenst@tut.by>
2022-08-21 14:50:09 +0300

    created method addStudents;

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int addedNumberOfStudents;
+
     public static void main(String[] args) {
         System.out.println("Learning Progress Tracker");
         String a;
@@ -11,8 +13,10 @@ public class Main {
             a = input();
             if (a.isBlank()) {
                 System.out.println("No input");
-            } else if(!"exit".equals(a)) {
+            } else if (!"exit".equals(a)) {
                 System.out.println("Unknown command!");
+            } else if ("add students".equals(input())) {
+                addStudents();
             }
         } while(!"exit".equals(a));
         System.out.println("Bye!");
@@ -27,5 +31,20 @@ public class Main {
         }
         return input;
     }
+
+    private static void addStudents() {
+        addedNumberOfStudents = 0;
+        String input;
+        do {
+            System.out.println("Enter student credentials or 'back' to return:");
+            input = input();
+            if ("back".equals(input)) {
+                System.out.printf("Total %d students have been added.", addedNumberOfStudents);
+            }
+        } while (!"back".equals(input));
+    }
+    /*private static boolean isStudent(String input) {
+
+    }*/
 
 }
