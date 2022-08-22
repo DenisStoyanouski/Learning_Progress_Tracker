4a980fe6344ef11020008e10c29ad57ffad79ef3
Denis Stoyanouski <vandenst@tut.by>
2022-08-22 19:51:02 +0300

    added isEmailCorrect method with tests

--- test/tracker/MainTest.java
+++ test/tracker/MainTest.java
@@ -10,87 +10,77 @@ class MainTest {
     @Test
     @DisplayName("firstName Jean-Clause")
     void testFirstName1() {
-        Main main = new Main();
         String name = "Jean-Clause";
-        assertTrue(main.isFirstNameCorrect(name));
+        assertTrue(isFirstNameCorrect(name));
     }
 
     @Test
     @DisplayName("firstName Je")
     void testFirstName2() {
-        Main main = new Main();
         String name = "Je";
-        assertTrue(main.isFirstNameCorrect(name));
+        assertTrue(isFirstNameCorrect(name));
     }
 
     @Test
     @DisplayName("firstName J")
     void testFirstName3() {
-        Main main = new Main();
+
         String name = "J";
-        assertFalse(main.isFirstNameCorrect(name));
+        assertFalse(isFirstNameCorrect(name));
     }
 
     @Test
     @DisplayName("firstName 陳")
     void testFirstName4() {
-        Main main = new Main();
         String name = "陳";
-        assertFalse(main.isFirstNameCorrect(name));
+        assertFalse(isFirstNameCorrect(name));
     }
 
     @Test
     @DisplayName("firstName O'Neal")
     void testFirstName5() {
-        Main main = new Main();
         String name = "O'Neal";
-        assertTrue(main.isFirstNameCorrect(name));
+        assertTrue(isFirstNameCorrect(name));
     }
 
     @Test
     @DisplayName("firstName O''Neal with double apostrophes")
     void testFirstName6() {
-        Main main = new Main();
         String name = "O''Neal";
-        assertFalse(main.isFirstNameCorrect(name));
+        assertFalse(isFirstNameCorrect(name));
     }
 
     @Test
     @DisplayName("firstName Jean--Clause with double hyphens")
     void testFirstName7() {
-        Main main = new Main();
         String name = "O''Neal";
-        assertFalse(main.isFirstNameCorrect(name));
+        assertFalse(isFirstNameCorrect(name));
     }
 
     @Test
     @DisplayName("firstName -Jean hyphen isn't at first place")
     void testFirstName8() {
-        Main main = new Main();
         String name = "-Jean";
-        assertFalse(main.isFirstNameCorrect(name));
+        assertFalse(isFirstNameCorrect(name));
     }
 
     @Test
     @DisplayName("firstName Jean- hyphen isn't at last place")
     void testFirstName9() {
-        Main main = new Main();
         String name = "Jean-";
-        assertFalse(main.isFirstNameCorrect(name));
+        assertFalse(isFirstNameCorrect(name));
     }
 
     @Test
     @DisplayName("firstName 'Neal apostrophes isn't at first place")
     void testFirstName10() {
-        Main main = new Main();
         String name = "'Neal";
-        assertFalse(main.isFirstNameCorrect(name));
+        assertFalse(isFirstNameCorrect(name));
     }
 
     @Test
     @DisplayName("firstName Neal' apostrophes isn't at last place")
     void testFirstName11() {
-        Main main = new Main();
         String name = "Neal'";
         assertFalse(isFirstNameCorrect(name));
     }
@@ -98,7 +88,6 @@ class MainTest {
     @Test
     @DisplayName("lastName Je Jean-Clause O'Neal")
     void testLastName1() {
-        Main main = new Main();
         String name = "Je Jean-Clause O'Neal";
         assertTrue(isLastNameCorrect(name));
     }
@@ -106,9 +95,29 @@ class MainTest {
     @Test
     @DisplayName("lastName Robert Jemison Van de Graaff or John Ronald Reuel Tolkien")
     void testLastName2() {
-        Main main = new Main();
         String name = "Robert Jemison Van de Graaff or John Ronald Reuel Tolkien";
         assertTrue(isLastNameCorrect(name));
+    }
+
+    @Test
+    @DisplayName("jdoe@mail.net")
+    void testEmail1() {
+        String email = "jdoe@mail.net";
+        assertTrue(isEmailCorrect(email));
+    }
+
+    @Test
+    @DisplayName("jane.doe@yahoo.com")
+    void testEmail2() {
+        String email = "jane.doe@yahoo.com";
+        assertTrue(isEmailCorrect(email));
+    }
+
+    @Test
+    @DisplayName("email")
+    void testEmail3() {
+        String email = "email";
+        assertFalse(isEmailCorrect(email));
     }
 
 
