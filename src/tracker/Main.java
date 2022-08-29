ecba79a6cd9e1fa9573cb34a5739f37094e8901a
Denis Stoyanouski <vandenst@tut.by>
2022-08-29 13:13:25 +0300

    added method addPoints

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -4,7 +4,9 @@ import java.util.*;
 
 public class Main {
 
-    static int addedNumberOfStudents;
+    static String lastName;
+    static String firstName;
+    static String email;
 
     private static Map<Integer, Student> studentList = new HashMap<>();
     private static Deque<Integer> idList = new ArrayDeque<>();
@@ -24,6 +26,12 @@ public class Main {
                 addStudents();
             } else if ("back".equals(a)){
                 System.out.println("Enter 'exit' to exit the program.");
+            } else if ("list".equals(a)) {
+                printIdList();
+            } else if ("add points".equals(a)) {
+                addPoints();
+            } else if ("find".equals(a)) {
+                findStudent();
             } else if (!"exit".equals(a)) {
                 System.out.println("Unknown command!");
             }
@@ -37,32 +45,27 @@ public class Main {
     }
 
     private static void addStudents() {
-        addedNumberOfStudents = 0;
         String command;
         System.out.println("Enter student credentials or 'back' to return:");
         do {
             command = input();
             if ("back".equals(command)) {
-                System.out.printf("Total %d students have been added.%n", addedNumberOfStudents);
-            } else if ("list".equals(command)) {
-                printIdList();
+                System.out.printf("Total %d students have been added.%n", studentList.size());
             } else {
-                if (isCorrectCredentials(command)) {
-                    System.out.println("The student has been added.");
-                    addedNumberOfStudents++;
-
+                if (isCorrectCredentials(getCredentials(command))) {
+                    addStudentToList(firstName, lastName,email);
                 }
             }
         } while (!"back".equals(command));
     }
 
-    protected static boolean isCorrectCredentials(String credentials) {
-        boolean isCorrectCredentials = false;
-        String firstName = "";
-        String lastName = "";
-        String email = "";
-
+    protected static String[] getCredentials(String credentials) {
         String[] credentialsOfStudent = credentials.split("\\s+");
+       return credentialsOfStudent;
+    }
+
+    protected static boolean isCorrectCredentials(String[] credentialsOfStudent) {
+        boolean isCorrectCredentials = false;
 
         if (credentialsOfStudent.length < 3) {
             System.out.println("Incorrect credentials");
@@ -77,9 +80,6 @@ public class Main {
             lastName = lastNameB.deleteCharAt(lastNameB.length() - 1).toString();
             isCorrectCredentials = isFirstNameCorrect(firstName) && isLastNameCorrect(lastName) && isEmailCorrect(email);
         }
-        if (isCorrectCredentials) {
-            addStudentToList(firstName, lastName, email);
-        }
 
         return isCorrectCredentials;
     }
@@ -88,11 +88,12 @@ public class Main {
         Student student = new Student(firstName, lastName, email);
         int id = 10000;
         if (!studentList.containsValue(student)) {
-            while(!studentList.containsKey(id)) {
+            while(studentList.containsKey(id)) {
                 id++;
             }
             studentList.put(id, student);
             idList.add(id);
+            System.out.println("The student has been added.");
         } else {
             System.out.println("This email is already taken.");
         }
@@ -101,8 +102,68 @@ public class Main {
     protected static void printIdList() {
         System.out.println("Students:");
         while (!idList.isEmpty()) {
-            System.out.println(idList.pollLast());
+            System.out.println(idList.pollFirst());
         }
+    }
+
+    protected static void addPoints() {
+        System.out.println("Enter an id and points or 'back' to return:");
+        String[] points;
+        do {
+            points = input().split("\\s+");
+            if ("back".equals(points[0])) {
+                break;
+            } else {
+                checkPoints(points);
+            }
+
+        } while (!"back".equals(points[0]));
+
+    }
+
+    protected static boolean checkPoints(String[] points) {
+        boolean isCorrect = false;
+        if (isPointsFormatCorrect(points) && isIdExist(points[0])) {
+
+        }
+        return isCorrect;
+    }
+    protected static boolean isPointsFormatCorrect(String[] points) {
+        boolean isCorrect = false;
+        for(int i = 1; i < points.length; i++) {
+            try {
+                int point = Integer.parseInt(points[i]);
+                if (point < 0) {
+                    System.out.println("Incorrect points format.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Incorrect points format.");
+            }
+        }
+
+
+
+        return isCorrect;
+    }
+
+
+    protected static boolean isIdExist(String id) {
+        boolean isIdExist = false;
+        int checkId = Integer.parseInt(id);
+        if (studentList.containsKey(checkId)) {
+            isIdExist = true;
+        }
+        return isIdExist;
+    }
+
+    protected static void findStudent() {
+        System.out.println("Enter an id and points or 'back' to return:");
+        String line;
+        do {
+            line = input();
+            if ("add".equals(line)) {}
+        } while (!"back".equals(line));
+
     }
 
     /*Accept only ASCII characters, from A to Z and from a to z as well as hyphens and apostrophes,
@@ -148,7 +209,7 @@ public class Main {
     }
 }
 
-class Student implements Comparable<Student>{
+class Student {
     String firstName;
     String lastName;
     String email;
@@ -160,8 +221,21 @@ class Student implements Comparable<Student>{
     }
 
     //Check if the provided email has been already used when adding information about students.
+
     @Override
-    public int compareTo(Student o) {
-        return this.email.compareTo(o.email);
+    public int hashCode() {
+        return Double.valueOf(email).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj instanceof Student) {
+            Student student =  (Student)obj;
+            return this.email.equals(student.email);
+        }
+        return false;
     }
 }
